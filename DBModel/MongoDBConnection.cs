b36a70cddb24288cb4f11ee2547dc33e055ffa2c
JuanAn6 <juanantoniorizija666@gmail.com>
2025-02-25 19:44:34 +0100

    25-02-2025

---
+++ DBModel/MongoDBConnection.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Text.Json;
+
+namespace DBModel
+{
+    public class MongoDBConnection
+    {
+        private static IMongoDatabase database;
+        private static MongoClient client = null;
+
+        public MongoDBConnection()
+        {
+            
+            string connectionString = "mongodb://localhost:27017";
+            if(client == null)
+            {
+                client = new MongoClient(connectionString);
+                database = client.GetDatabase("project");
+            }
+
+        }
+
+        public IMongoCollection<BsonDocument> GetCollection(string collectionName)
+        {
+            return database.GetCollection<BsonDocument>(collectionName);
+        }
+    }
+}
---
+++ ProjecteBotigaSabates/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using DBModel;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjecteBotigaSabates
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        MongoDBConnection mongoDBConnection;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            mongoDBConnection = new MongoDBConnection();
+
+            IMongoCollection<BsonDocument> empresa = mongoDBConnection.GetCollection("dades_empresa");
+            foreach(BsonDocument doc in empresa.Find(new BsonDocument()).ToList()) {
+                Debug.WriteLine(doc.ToJson());
+            }
+        }
+    }
+}

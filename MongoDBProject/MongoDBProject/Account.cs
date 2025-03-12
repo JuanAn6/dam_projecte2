4cd9f3b6abcb0beb71c85a50364eba245643b21a
JuanAn6 <juanantoniorizija666@gmail.com>
2025-03-12 20:22:52 +0100

    12-03-2025

---
+++ MongoDBProject/MongoDBProject/Account.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDBProject
+{
+    internal class Account
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("account_id")]
+        public string AccountId { get; set; }
+
+        [BsonElement("account_holder")]
+        public string AccountHolder { get; set; }
+
+        [BsonElement("account_type")]
+        public string AccountType { get; set; }
+
+        [BsonRepresentation(BsonType.Decimal128)]
+        [BsonElement("balance")]
+        public decimal Balance { get; set; }
+
+        [BsonElement("transfers_complete")]
+        public string[] TransfersCompleted { get; set; }
+    }
+}
---
+++ MongoDBProject/MongoDBProject/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MongoDB.Driver;
+
+var mongodbUrl = "mongodb+srv://juan:juan1234@cluster0.oqsm0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+var client = new MongoClient(mongodbUrl);
+
+var dbList = client.ListDatabases().ToList();
+
+Console.WriteLine("The list of databases on this server is: ");
+
+foreach (var db in dbList)
+{
+    Console.WriteLine(db);
+}

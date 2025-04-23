6549d292c1e4540ac6e25e4839bf5d136a1e90d1
JuanAn6 <juanantoniorizija666@gmail.com>
2025-04-23 17:37:56 +0200

    23-04-2025

--- ProjecteBotigaSabates/DBModel/obj/Debug/net8.0/DBModel.AssemblyInfo.cs
+++ ProjecteBotigaSabates/DBModel/obj/Debug/net8.0/DBModel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DBModel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a7a1dd5b4a58423e85229ec208b76555179541a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebd53f7cdffe7f3cc8a698cff617a790e2f7d956")]
 [assembly: System.Reflection.AssemblyProductAttribute("DBModel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DBModel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
